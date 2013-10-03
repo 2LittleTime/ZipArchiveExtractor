ce8b2aba9fa942c57dc7abcf450b7ccf17f0801f
2LittleTime <jmccabe@austin.rr.com>
2013-10-03 14:53:44 -0500

    new repo creation for zip archive extractor - 20131003

---
+++ ExtractQclcdFiles/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace ExtractQclcdFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define array of zip file names
+            string[] filesArray = Directory.GetFiles(@"F:\QCLCD_RawData");
+
+            Console.WriteLine("--QCLCD Files--");
+            
+            //Loop through all the zip archive files
+            foreach (string fileName in filesArray)
+            {
+                Console.WriteLine("File name is: {0}", fileName.ToString());
+
+                //Define the extract point
+                string extractPath = @"F:\QCLCD_RawData\extract";
+
+                //Open the zipArchive file
+                using (ZipArchive archive = ZipFile.OpenRead(fileName.ToString()))
+                {
+                    //Loop through the entries in the archive
+                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    {
+                        Console.WriteLine("Entry {0}", entry.FullName.ToString());
+
+                        //extract the *daily.txt files and copy to the extract directory
+                        if (entry.FullName.EndsWith("daily.txt", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine("DailyFile is {0}", entry.FullName.ToString());
+                            entry.ExtractToFile(Path.Combine(extractPath, entry.FullName));
+                            Console.WriteLine("DailyFile extracted..", entry.FullName.ToString());
+                        }
+
+                    }
+                    
+                } 
+                
+
+            }
+
+            Console.WriteLine("Press enter to finish.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,13 @@
+**************
+Zip File Archive Extractor
+**************
+
+Blake Mccabe - 20131003
+
+The purpose of this small app is to extract files of a defined file name pattern from a zip archive folder to another folder.
+
+It was built for the UsWeather Summary app (http://usweathersummary.azurewebsites.net) as the process of moving approximately 72 files from the QCLCD arvhives folders by hand was very tedious and error prone.
+
+By resetting the values in the origin path, file matching, and destination strings, anyone can use the app to extract the desired files from zip archive folders programmatically.
+
+  
